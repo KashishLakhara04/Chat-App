98e33fdefb05e197a0bc3d4cc2231c095f92a39d
KashishLakhara04 <kashishlakhara04@gmail.com>
2022-09-07 13:44:29 +0530

    edit: labels updated

--- src/chatting/application/Server.java
+++ src/chatting/application/Server.java
@@ -4,13 +4,13 @@ import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-public class Server extends JFrame implements ActionListener {
+
+public class Server extends JFrame {//implements ActionListener {
 
     JTextField text;
     JPanel a1;
